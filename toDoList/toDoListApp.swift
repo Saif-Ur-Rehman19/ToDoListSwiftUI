1b3a2dcae1a957cda759b70e92c6cd19d517ccb1
Saif-Ur-Rehman-19 <saifurrehmanparwak123@gmail.com>
2024-07-10 23:01:21 +0500

    Initial Commit

---
+++ toDoList/toDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoListApp.swift
+//  toDoList
+//
+//  Created by uExel on 10/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct toDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
